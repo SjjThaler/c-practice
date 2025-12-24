a05595cdcad73c1b6c17abf995f5cc862b17fb87
bidiu <stefanthaler@protonmail.com>
2025-12-24 14:09:07 +0100

    Added memory-leak experiment

---
+++ memory_experiments/memory_leak.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int memory_leak() {
+    int *ptr = malloc(1000 * sizeof(int)); // size of int == 4 bytes * 1000
+    *ptr = 100;
+    return 0;
+    // doesn't free the heap and leaves a 4 byte orphan
+}
+
+int main() {
+    for (int i = 0; i < 1000000; i++) { // 1 million * 4000 bytes
+        memory_leak(); // approximately 4G of orphans
+    }
+    printf("Approximately 3 GB RAM leaked! Press any key to exit... \n");
+    getchar();
+    return 0;
+}
